0008b4fb4e8593580251f36f5a41ba9d005b5311
Beregszászi István <beistvan@gmail.com>
2023-06-28 07:59:21 +0200

    Hello World Flutter 2 Dart

---
+++ hello-flutter.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Simple',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("App Bar Title'),
+        ), // AppBar(
+        body: Center(
+          child: Text('Hello Flutter!'),
+        ), // Center
+      ), // Scaffold
+    ), // MaterialApp
+  }
+}
